d7f3e7efc1f7f9ba524bdf8c09545114d431f8cf
Vezir Oni <vezironi@yandex.com>
2024-10-18 12:40:33 +0300

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './components/**/*.{html,js}',
     './pages/**/*.{html,js}',
     './app/**/*.{html,js}',
   ],
-  // ...
 }
--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,45 @@
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
+ 
 export default {
     logo: <div style={{"paddingLeft": "47px", "lineHeight": "38px", "background": "url('https://i.hizliresim.com/jdw864z.png') no-repeat left", "backgroundSize": "36px", "fontWeight": "550"}}>HDStore</div>,
     project: {
       link: 'https://github.com/harbiyelidev/hdstore-docs'
+    },
+    head() {
+      const { asPath, defaultLocale, locale } = useRouter()
+      const { frontMatter } = useConfig()
+      const url =
+        'https://docs.hdstore.info' +
+        (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+   
+      return (
+        <>
+          <meta name="description" content="A store that produces premium scenarios for a game called FiveM!" />
+          <meta name="google-site-verification" content="googleb14168684a72b411.html" />
+          <meta name="robots" content="noindex,nofollow" />
+          <meta property="og:url" content={url} />
+          <meta property="og:site_name" content="HDStore" />
+          <meta property="og:title" content={frontMatter.title || 'HDStore Docs'} />
+          <meta
+            property="og:description"
+            content={frontMatter.description || 'A store that produces premium scenarios for a game called FiveM!'}
+          />
+          <meta property="og:image" content="https://i.imgur.com/rpv9yPd.jpeg" />
+          <meta property="og:image:secure_url" content="https://i.imgur.com/rpv9yPd.jpeg" />
+          <meta property="og:image:type" content="image/jpeg" />
+          <meta property="og:image:width" content="1920" />
+          <meta property="og:image:height" content="1080" />
+          <meta property="og:image:alt" content="A store that produces premium scenarios for a game called FiveM!" />
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:site" content="HDStore Docs" />
+          <meta name="twitter:creator" content="@vezironi" />
+          <meta property="og:url" content={url} />
+          <meta property="og:title" content={frontMatter.title || 'HDStore Docs'} />
+          <meta property="og:description" content={frontMatter.description || 'A store that produces premium scenarios for a game called FiveM!'} />
+          <meta property="og:image" content="https://i.imgur.com/rpv9yPd.jpeg" />
+        </>
+     )
     },
     useNextSeoProps() {
       return {
@@ -33,10 +71,11 @@ export default {
         }
     },
     footer: {
-        content: (
-            <span>
-                Copyright © HDStore 2024. All Rights Reserved.
-            </span>
-        )
-      }
+      content: (
+          <span>
+              Copyright © HDStore 2024. All Rights Reserved.
+          </span>
+      )
+    },
+    faviconGlyph: '🐍'
 }
