7c76b1b05d9263c792fc89ce4f6128210621b0e0
Vezir Oni <vezironi@yandex.com>
2024-10-18 16:14:45 +0300

    🚀Update @harbiyelidev

--- theme.config.jsx
+++ theme.config.jsx
@@ -14,37 +14,28 @@ export default {
         <meta name="theme-color" content="#000000" />
         <meta
           name="description"
-          content={
-            frontMatter.description ||
-            'Discover the top-rated FiveM scripts and mods at HD Store. Secure, high-performance ESX and QBCore resources tailored for your server. Unlock exclusive Tebex deals today!'
-          }
+          content='Discover the top-rated FiveM scripts and mods at HD Store. Secure, high-performance ESX and QBCore resources tailored for your server. Unlock exclusive Tebex deals today!'
         />
         <meta name="author" content="HarbiyeliDev" />
         <meta
           name="keywords"
           content="Top FiveM Scripts, Buy ESX Scripts, QBCore Mods, Secure Tebex Mods, Premium FiveM Deals, Custom FiveM Server Scripts, Roleplay Mods, Tebex Store, High-Performance GTA V Mods, HDStore, Tebex Fivem, Fivem Store, Docs, HDStore Docs"
         />
-        <meta property="og:url" content={url} />
+        <meta property="og:url" content='https://docs.hdstore.info' />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="HDStore" />
         <meta
           property="og:title"
-          content={frontMatter.title || 'HDStore Docs'}
+          content='HDStore Docs'
         />
         <meta
           property="og:description"
-          content={
-            frontMatter.description ||
-            'Discover the top-rated FiveM scripts and mods at HD Store. Secure, high-performance ESX and QBCore resources tailored for your server. Unlock exclusive Tebex deals today!'
-          }
+          content='Discover the top-rated FiveM scripts and mods at HD Store. Secure, high-performance ESX and QBCore resources tailored for your server. Unlock exclusive Tebex deals today!'
         />
         <meta property="og:image" content="https://i.imgur.com/rpv9yPd.jpeg" />
         <meta
           property="og:image:alt"
-          content={
-            frontMatter.description ||
-            'Discover the top-rated FiveM scripts and mods at HD Store. Secure, high-performance ESX and QBCore resources tailored for your server. Unlock exclusive Tebex deals today!'
-          }
+          content='Discover the top-rated FiveM scripts and mods at HD Store. Secure, high-performance ESX and QBCore resources tailored for your server. Unlock exclusive Tebex deals today!'
         />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:creator" content="@vezironi" />
